3a8955755e07065bbb5d73526a36414b9a1613b0
Manuel Ruck <github@manuelruck.de>
2018-10-04 07:40:26 +0200

    repair react-native 0.57

--- packages/app/rn-cli.config.js
+++ packages/app/rn-cli.config.js
@@ -1,14 +1,6 @@
 const path = require('path');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
-alternateRoots = [
-  // path.resolve(__dirname),
-  path.resolve(__dirname, "./node_modules"),
-  // path.resolve(__dirname, "../common"),
-  path.resolve(__dirname, "../../node_modules"),
-  path.resolve(__dirname, "../.."),
-];
-
 module.exports = {
     transformer: {
       babelTransformerPath: require.resolve("react-native-typescript-transformer")
--- packages/web/server/index.ts
+++ packages/web/server/index.ts
@@ -9,17 +9,19 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
+    if (req.url) {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query);
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query);
-    } else {
-      handle(req, res, parsedUrl);
+      if (pathname === '/a') {
+        app.render(req, res, '/a', query);
+      } else if (pathname === '/b') {
+        app.render(req, res, '/b', query);
+      } else {
+        handle(req, res, parsedUrl);
+      }
     }
-  }).listen(port, err => {
+  }).listen(port, (err: any) => {
     if (err) throw err;
     // tslint:disable-next-line:no-console
     console.log(`> Ready on http://localhost:${port}`);
