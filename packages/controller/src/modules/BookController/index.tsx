3a9cfe49035edf5fb8218f45011a46b44c924239
Manuel Ruck <github@manuelruck.de>
2018-10-10 10:19:10 +0200

    remove comments

--- packages/controller/src/modules/BookController/index.tsx
+++ packages/controller/src/modules/BookController/index.tsx
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import * as React from 'react';
+import React from 'react';
 import { Query } from 'react-apollo';
 
 import { BookListQuery, BookListQuery_books } from '../../schemaTypes';
@@ -13,29 +13,14 @@ const bookListQuery = gql`
   }
 `;
 
-// interface IProps {
-//   children: (data: any) => JSX.Element | null;
-// }
-
 export interface IBookControllerProps {
   data: { books: BookListQuery_books[] };
   loading: boolean;
   error: any;
 }
 
-// export const BookController = graphql<any, BookListQuery, {}, IBookList>(
-//   bookListQuery,
-//   {
-//     props: ({ data: { loading, books } }) => {
-//       return {
-//         books,
-//         loading,
-//       };
-//     },
-//   },
-// );
-
 interface IProps {
+  // children: ReactNode;
   children: any;
 }
 
