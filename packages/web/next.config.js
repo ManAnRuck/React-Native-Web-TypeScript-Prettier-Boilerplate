755c0209b06fe3223e47fc12e06dfb344f3b8efa
Manuel Ruck <github@manuelruck.de>
2018-11-18 16:33:03 +0100

    fix (web) babel core error

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,9 +2,17 @@ const withPlugins = require("next-compose-plugins");
 const withTM = require("next-plugin-transpile-modules");
 const withTypescript = require("@zeit/next-typescript");
 
+const nextConfig = {
+    webpack: (config, options) => {
+      // modify the `config` here
+      // console.log("CONFIG",config)
+      return config;
+    },
+  };
+
 module.exports = withPlugins([
  [withTM, {
   transpileModules: ["@myproject/common"],
  }],
  [withTypescript],
-]);
+], nextConfig);
