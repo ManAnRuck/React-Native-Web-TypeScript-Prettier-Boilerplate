e473932149cf5ad437ee0d9af46b58b2fe5bf571
Manuel Ruck <github@manuelruck.de>
2019-02-11 14:23:11 +0100

    fix logout test

--- packages/web/cypress/integration/login.spec.ts
+++ packages/web/cypress/integration/login.spec.ts
@@ -10,9 +10,19 @@ describe('login', () => {
       cy.get('[name=password]').type('my-test-password');
     });
     cy.get('[data-testid=register-form]').submit();
+    cy.get('[data-testid=logout-button]');
   });
 
   it('click logout button', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('[data-testid=login-button]').click();
+
+    cy.get('[data-testid=register-form]').within(() => {
+      cy.get('[name=email]').type('cypress2@test.de');
+      cy.get('[name=password]').type('my-test-password');
+    });
+    cy.get('[data-testid=register-form]').submit();
     cy.get('[data-testid=logout-button]').click();
+    cy.get('[data-testid=login-button]');
   });
 });
