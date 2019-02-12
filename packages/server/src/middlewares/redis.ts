425ee61f5f2e05730c7417f9b913e7b77a029d7a
Manuel Ruck <github@manuelruck.de>
2019-02-12 12:30:20 +0100

    remove log from redis

--- packages/server/src/middlewares/redis.ts
+++ packages/server/src/middlewares/redis.ts
@@ -9,43 +9,40 @@ const SESSION_SECRET = config.get('auth.sessionSecret');
 
 const router: Router = Router();
 const RedisStore = connectRedis(session as any);
-try {
-  export const redis =
-    process.env.NODE_ENV === 'production'
-      ? new Redis(config.get('db.redisUrl'))
-      : new Redis();
 
-  router.use((req, _, next) => {
-    const authorization = req.headers.authorization;
-    if (authorization) {
-      try {
-        const qid = authorization.split(' ')[1];
-        req.headers.cookie = `qid=${qid}`;
-      } catch (_) {
-        // do nothing
-      }
+export const redis =
+  process.env.NODE_ENV === 'production'
+    ? new Redis(config.get('db.redisUrl'))
+    : new Redis();
+
+router.use((req, _, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    try {
+      const qid = authorization.split(' ')[1];
+      req.headers.cookie = `qid=${qid}`;
+    } catch (_) {
+      // do nothing
     }
+  }
 
-    return next();
-  });
+  return next();
+});
 
-  router.use(
-    session({
-      store: new RedisStore({
-        client: redis as any,
-      }),
-      name: 'qid',
-      secret: SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        // secure: process.env.NODE_ENV === 'production', // TODO make cookie secure on live!
-        maxAge: 1000 * 60 * 60 * 7, // 7 days
-      },
-    } as any),
-  );
-} catch (error) {
-  console.error('REDIS', error);
-}
+router.use(
+  session({
+    store: new RedisStore({
+      client: redis as any,
+    }),
+    name: 'qid',
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      // secure: process.env.NODE_ENV === 'production', // TODO make cookie secure on live!
+      maxAge: 1000 * 60 * 60 * 7, // 7 days
+    },
+  } as any),
+);
 export default router;
