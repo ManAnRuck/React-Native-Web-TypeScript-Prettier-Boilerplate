27fb428bbafc02f65c62e79e8679c15b644457a7
Manuel Ruck <manuel.ruck@vodafone.com>
2019-06-29 15:18:15 +0200

    upgrade react native

--- packages/app/rn-cli.config.js
+++ packages/app/rn-cli.config.js
@@ -2,12 +2,16 @@ const path = require('path');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-typescript-transformer")
-    },
-    watchFolders: [
-      path.resolve(__dirname, "../.."),
-      ],
-  };
+  transformer: {
+    babelTransformerPath: require.resolve(
+      'react-native-typescript-transformer',
+    ),
+  },
+  watchFolders: [
+    // Keep your project directory.
+    path.resolve(__dirname),
 
-  
+    // Include arc/root directory as a new root.
+    path.resolve(__dirname, '../..'),
+  ],
+};
